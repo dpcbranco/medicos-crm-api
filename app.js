64295d76777c40a62ba5bce945e4b339721e9240
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-11-09 20:21:31 -0300

    Corrige path da API

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/medico', medicosRouter);
+app.use('/medicos', medicosRouter);
 
 module.exports = app;
