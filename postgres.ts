580388f00b11ad2a257962cb78cbf22ffa1594df
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-11-10 00:51:07 -0300

    Altera formato para ES6

--- postgres.ts
+++ postgres.ts
@@ -1,16 +1,20 @@
-const { Pool } = require('pg');
+import { Pool } from 'pg';
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
 
-const connect = () => {
+export const connect = () => {
     return pool.connect((err) => {
         if (err) throw err;
     });
 };
 
-const query = async (query: string, values?: Array<string|number>) => {
+export const query = async (
+    query: string,
+    values?: Array<string|number>
+) => {
     return await pool
         .query(query, values)
         .then((res) => {
@@ -19,8 +23,4 @@ const query = async (query: string, values?: Array<string|number>) => {
         .catch((err) => {
             return err;
         });
-};
-
-module.exports = {
-    connect, query
 };
