4d7faa422318a4d976fa42ae9b7691d327986d43
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-11-15 14:25:25 -0300

    [Hotfix] Altera import do fetch e biblioteca

--- services/consultaCrm.ts
+++ services/consultaCrm.ts
@@ -1,4 +1,4 @@
-import fetch from 'cross-fetch';
+import fetch from 'node-fetch';
 import { ApiError } from '../types/ApiError';
 import { ConsultaCrmResponse } from '../types/ConsultaCrmResponse';
 import { Medico } from '../types/Medico';
