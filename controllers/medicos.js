e021302b3af4eb97d85d183137c12c97f2e16f42
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-11-21 17:22:16 -0300

    Remove uso da API externa

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -1,4 +1,4 @@
-const { getMedicoByCrm } = require('../services/database');
+const { getMedicoByCrm, insertMedico } = require('../services/database');
 const { UF } = require('../types/UF');
 const { ApiError } = require('../types/ApiError');
 const { searchByCrm } = require('../services/consultaCrm');
@@ -21,18 +21,31 @@ const getMedico = async (
         return res.status(500).send({ error: dbResponse.message });
     }
     if (dbResponse.length === 0){
-        const apiResponse = await searchByCrm(crm, uf);
-        if (apiResponse instanceof ApiError) {
-            if (apiResponse.status === 500)
-                console.error(apiResponse)
-            return res
-                .status(apiResponse.status ?? 500)
-                .send({ error: apiResponse.error });
-        }
-        return res.send(apiResponse);
+        res.status(404).send({ error: 'File not found' })
     }
     
     return res.send(dbResponse[0]);
 };
 
-module.exports = { getMedico };
+const createMedico = async (req, res) => {
+    if (!req.body.nome)
+        return res.status(400).send({ error: 'Nome do médico não informado' })
+    if (!req.body.crm)
+        return res.status(400).send({ error: 'CRM do médico não informado' })
+    if (!req.body.uf)
+        return res.status(400).send({ error: 'UF do CRM não informada' })
+    if (!UF[req.body.uf])
+        return res.status(400).send({ error: 'UF inválida' });
+    if (isNaN(Number(req.body.crm)))
+        return res.status(400).send({ error: 'CRM inválido' });
+
+    const medicoDB = await getMedicoByCrm(req.body.crm, req.body.uf);
+    if (medicoDB.length > 0)
+        return res.status(409).send({ error: "Médico já cadastrado na base" });
+    const dbResponse = await insertMedico(req.body);
+    if (dbResponse.severity === 'ERROR')
+        return res.status(500).send(dbResponse)
+    return res.send({ id: dbResponse[0].id })
+}
+
+module.exports = { getMedico, createMedico };
